cab02fe5595ce8db6c0fa30c19c0c14aa524330e
myjhye <t01087633799@gmail.com>
2024-04-02 23:38:17 +0900

    경로 보호

--- pages/post/new.js
+++ pages/post/new.js
@@ -1,7 +1,27 @@
-export default function NewPost() {
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import { AppLayout } from "../../components/AppLayout";
+
+export default function NewPost(props) {
+
+    console.log(props);
+
     return (
         <div>
             new post!!
         </div>
     )
 }
+
+NewPost.getLayout = function getLayout(page, pageProps) {
+    return (
+        <AppLayout {...pageProps}>
+            {page}
+        </AppLayout>
+    )
+}
+
+export const getServerSideProps = withPageAuthRequired(() => {
+    return {
+        props: {},
+    };
+});
