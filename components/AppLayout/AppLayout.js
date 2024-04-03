e9878dc7132e3797fcc8bfd989a747a5f14e50f0
myjhye <t01087633799@gmail.com>
2024-04-04 02:19:30 +0900

    logo

--- components/AppLayout/AppLayout.js
+++ components/AppLayout/AppLayout.js
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from "next/image";
+import { Logo } from '../Logo';
 
 export const AppLayout = ({ children }) => {
 
@@ -12,9 +13,16 @@ export const AppLayout = ({ children }) => {
         <div className="grid grid-cols-[300px_1fr] h-screen max-h-screen">
             <div className="flex flex-col text-white overflow-hidden">
                 <div className="bg-slate-800">
-                    <div>logo</div>
-                    <div>cta button</div>
-                    <div>tokens</div>
+                    <Logo />
+                    <Link 
+                        href="/post/new"
+                        className="bg-green-500 tracking-wider w-full text-center text-white font-bold cursor-pointer uppercase px-4 py-2 rounded-md hover:bg-green-600 transition-colors block"
+                    >
+                        new post
+                    </Link>
+                    <Link href="/token-topup">
+                        0 tokens available
+                    </Link>
                 </div>
                 <div className="flex-1 overflow-auto bg-gradient-to-b from-slate-800 to-cyan-800">
                     list of posts
