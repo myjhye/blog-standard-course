8feff16af4929799c6dbe552ab34a4c03fcde50e
myjhye <t01087633799@gmail.com>
2024-04-04 02:08:26 +0900

    폰트 설정

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,19 @@
 
 import '../styles/globals.css'
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { DM_Sans, DM_Serif_Display } from '@next/font/google';
+
+const dmSans = DM_Sans({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+  variable: "--font-dm-sans",
+});
+
+const dmSerifDisplay = DM_Serif_Display({
+  weight: ['400'],
+  subsets: ['latin'],
+  variable: "--font-dm-serif",
+});
 
 function MyApp({ Component, pageProps }) {
 
@@ -10,7 +23,9 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <UserProvider>
-      {getLayout(<Component {...pageProps} />, pageProps)}
+      <main className={`${dmSans.variable} ${dmSerifDisplay.variable}`}>
+        {getLayout(<Component {...pageProps} />, pageProps)}
+      </main>
     </UserProvider>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    // tailwind css 적용할 파일 지정
     './app/**/*.{js,ts,jsx,tsx}',
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    // Or if using `src` directory:
+    // 'src' 디렉토리 사용 시 아래 경로도 포함
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {},
+  theme: {
+    // tailwind 기본 테마 설정 확장
+    extend: {
+      fontFamily: {
+        // 본문용 폰트, 헤딩용 폰트 설정 - css 변수 연결
+        body: 'var(--font-dm-sans)',
+        heading: 'var(--font-dm-serif)',
+      },
+    },
+  },
   plugins: [],
 };
