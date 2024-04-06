bb26ea136ba56a7ed86f7a3418019caefa7ec03a
myjhye <t01087633799@gmail.com>
2024-04-06 12:41:05 +0900

    api 요청하고 응답 화면에 표시

---
+++ pages/api/generatePost.js
@@ -0,0 +1,71 @@
+import { OpenAIApi, Configuration } from "openai"
+
+export default async function handler(req, res) {
+    const config = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const openai = new OpenAIApi(config);
+
+    const { topic, keywords } = req.body;
+
+    const response = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: "system",
+          content: "You are an SEO friendly blog post generator called BlogStadard. You are designed to output markdown without frontmatter.",
+        },
+        {
+          role: "user",
+          content: `
+            Generate me a long and detailed seo friendly blog post on the following topic delimited by triple hyhens:
+            ---
+            ${topic}
+            ---
+            Targeting the following comma separated keywords delimited by triple hyphens:
+            ---
+            ${keywords}
+            ---
+          `
+        }
+      ],
+    });
+
+    const postContent = response.data.choices[0]?.message?.content;
+    const seoResponse = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          {
+            role: "system",
+            content: "You are an SEO friendly blog post generator called BlogStadard. You are designed to output JSON. Do not include HTML tags in your output."
+          },
+          { 
+            role: "user",
+            content: `
+              Generate an SEO friendly title and SEO friendly meta description for the following blog post:
+              ${postContent}
+              ---
+              The output json must be in the following format:
+              {
+                "title": "example title",
+                "metaDescription": "example meta description"
+              }
+            `,
+          },
+        ],
+        response_format: { type: "json_object" },
+      });
+
+    const { title, metaDescription } = seoResponse.data.choices[0].message?.content || {};
+
+    res.status(200).json({ 
+      post: { 
+        postContent, 
+        title, 
+        metaDescription 
+      }, 
+    });
+}
+
+  
--- pages/post/new.js
+++ pages/post/new.js
@@ -1,11 +1,65 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../../components/AppLayout";
+import { useState } from "react";
+import Markdown from "react-markdown";
 
 export default function NewPost(props) {
 
+    const [topic, setTopic] = useState("");
+    const [keywords, setKeywords] = useState("");
+    const [postContent, setPostContent] = useState("");
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const response = await fetch(`/api/generatePost`, {
+            method: "POST",
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify({ topic, keywords }),
+        });
+        const json = await response.json();
+        console.log("Result: ", json);
+        setPostContent(json.post.postContent);
+    };
+
     return (
         <div>
-            new post!!
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>
+                        <strong>
+                            Generate a blog post on the topic of:
+                        </strong>
+                    </label>
+                    <textarea
+                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" 
+                        value={topic}
+                        onChange={(e) => setTopic(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label>
+                        <strong>
+                            Targeting the following keywords:
+                        </strong>
+                    </label>
+                    <textarea
+                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"  
+                        value={keywords}
+                        onChange={(e) => setKeywords(e.target.value)}
+                    />
+                </div>
+                <button
+                    type="submit" 
+                    className="btn"
+                >
+                    generate
+                </button>
+            </form>
+            <Markdown>
+                {postContent}
+            </Markdown>
         </div>
     )
 }
