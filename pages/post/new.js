5da1add5998ebaed3123765bbf9a65c1e667eed2
myjhye <t01087633799@gmail.com>
2024-04-08 22:14:46 +0900

    스타일링 - 질문 작성 페이지

--- pages/post/new.js
+++ pages/post/new.js
@@ -42,42 +42,50 @@ export default function NewPost(props) {
     };
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                {/* 질문 입력 */}
-                <div>
-                    <label>
-                        <strong>
-                            Generate a blog post on the topic of:
-                        </strong>
-                    </label>
-                    <textarea
-                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" 
-                        value={topic}
-                        onChange={(e) => setTopic(e.target.value)}
-                    />
-                </div>
-                {/* 키워드 입력 */}
-                <div>
-                    <label>
-                        <strong>
-                            Targeting the following keywords:
-                        </strong>
-                    </label>
-                    <textarea
-                        className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"  
-                        value={keywords}
-                        onChange={(e) => setKeywords(e.target.value)}
-                    />
-                </div>
-                {/* 게시물 생성 버튼 */}
-                <button
-                    type="submit" 
-                    className="btn"
+        <div className="h-full overflow-hidden">
+            <div className="w-full h-full flex flex-col overflow-auto">
+                <form 
+                    onSubmit={handleSubmit}
+                    className="m-auto w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200"
                 >
-                    generate
-                </button>
-            </form>
+                    {/* 질문 입력 */}
+                    <div>
+                        <label>
+                            <strong>
+                                Generate a blog post on the topic of:
+                            </strong>
+                        </label>
+                        <textarea
+                            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm" 
+                            value={topic}
+                            onChange={(e) => setTopic(e.target.value)}
+                        />
+                    </div>
+                    {/* 키워드 입력 */}
+                    <div>
+                        <label>
+                            <strong>
+                                Targeting the following keywords:
+                            </strong>
+                        </label>
+                        <textarea
+                            className="resize-none border border-slate-500 w-full block my-2 px-4 py-2 rounded-sm"  
+                            value={keywords}
+                            onChange={(e) => setKeywords(e.target.value)}
+                        />
+                        <small className="block mb-2">
+                            Separate keywords with a comma
+                        </small>
+                    </div>
+                    {/* 게시물 생성 버튼 */}
+                    <button
+                        type="submit" 
+                        className="btn"
+                    >
+                        generate
+                    </button>
+                </form>
+            </div>
         </div>
     )
 }
